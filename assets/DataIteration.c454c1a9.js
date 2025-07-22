5654f3be3bb901f899f97a59e0f34eb305c1b755
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-07-22 11:14:02 +0000

    REDIRECTION

---
+++ assets/DataIteration.c454c1a9.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.9afb2c41.js";function c(e){const{datas:t=[],startLength:r,endLength:n,children:s}=e;return a.jsx(a.Fragment,{children:t&&t.length>=n&&t.slice(r,n).map(i=>s({datas:i}))})}export{c as D};
